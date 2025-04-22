33aefbf0f95cdc51e242c77711c983f95961f3fa
husknet <127146284+husknet@users.noreply.github.com>
2025-04-22 08:38:06 -0300

    Update detect_bot.js

--- pages/api/detect_bot.js
+++ pages/api/detect_bot.js
@@ -5,6 +5,7 @@ import stringSimilarity from 'string-similarity';
 const KNOWN_BOT_ISPS = [
   
   "RGT/SMP",
+  "ftn tilepikoinonies mepe",
   "techoff srv limited",
   "cloudflarenet eu",
   "tzulo, inc.",
