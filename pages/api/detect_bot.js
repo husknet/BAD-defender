a8c07995671af9e6a9597d9f200b75b99843096e
husknet <127146284+husknet@users.noreply.github.com>
2025-06-16 06:57:06 -0300

    Update detect_bot.js

--- pages/api/detect_bot.js
+++ pages/api/detect_bot.js
@@ -5,6 +5,7 @@ import stringSimilarity from 'string-similarity';
 const KNOWN_BOT_ISPS = [
   
   "RGT/SMP",
+  "oracle public cloud",
   "level 3 parent, llc",
   "ovh hosting, inc.",
   "vpn consumer london, united kingdom",
