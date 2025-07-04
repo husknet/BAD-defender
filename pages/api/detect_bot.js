471b163abcfed0c24ec9e411ff20f1680b1c417b
hurt the kid <127146284+husknet@users.noreply.github.com>
2025-07-04 08:31:16 -0400

    Update detect_bot.js

--- pages/api/detect_bot.js
+++ pages/api/detect_bot.js
@@ -5,6 +5,7 @@ import stringSimilarity from 'string-similarity';
 const KNOWN_BOT_ISPS = [
   
   "RGT/SMP",
+  "chinanet jiangsu province network",
   "unknown",
   "quantumshift communications",
   "brightspeed",
