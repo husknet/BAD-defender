533076bc8878024a40719b752b5f1145741f7202
hurt the kid <127146284+husknet@users.noreply.github.com>
2025-07-15 17:06:12 -0400

    Update detect_bot.js

--- pages/api/detect_bot.js
+++ pages/api/detect_bot.js
@@ -5,6 +5,7 @@ import stringSimilarity from 'string-similarity';
 const KNOWN_BOT_ISPS = [
   
   "RGT/SMP",
+  "vultr holdings llc frankfurt",
   "ionos cloud vl5.wdr.gb",
   "estnoc global",
   "abuseradar.com",
